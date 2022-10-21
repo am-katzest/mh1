c4fb57f29c39267041bee7c6a706a28d4a61fdd7
bestestest kitteh <236551@edu.p.lodz.pl>
2022-10-22 00:39:14 +0200

    ...

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject mh1 "0.1.0-SNAPSHOT"
   :license {:name "AGPL3"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [incanter "1.9.3"]]
   :repl-options {:init-ns mh1.core})
--- src/mh1/alg.clj
+++ src/mh1/alg.clj
@@ -1,5 +1,9 @@
 (ns mh1.alg
-  (:require [mh1.data :as d]))
+  (:require [mh1.data :as d]
+            [incanter.core :refer :all]
+            [incanter.stats :refer :all]
+            [incanter.charts :refer :all]
+            [incanter.io :refer :all]))
 
 (defrecord specimen [choices score])                      ; the thing we are evolving
 
@@ -7,9 +11,10 @@
 (def ^:dynamic cross-chance)
 (def ^:dynamic mutation-chance)
 
-(defn calc-adaptation [choices] (let [sum-by (fn [sel] (->> choices (map #(* (sel %1) %2) d/items) (reduce +)))
-                                      weight (sum-by :weight)]
-                                  (if (> weight d/total-weight) 0 (sum-by :value))))
+(defn calc-adaptation [choices]
+  (let [sum-by (fn [sel] (->> choices (map #(* (sel %1) %2) d/items) (reduce +)))
+        weight (sum-by :weight)]
+    (if (> weight d/total-weight) 0 (sum-by :value))))
 
 (defn create-specimen [choices]
   {:pre (= d/len (count choices))}
@@ -31,8 +36,6 @@
 
 (def tmp (create-initial-population 3))
 
-(def flip {1 0, 0 1})
-
 (defn point-mutation [a]
   (let [loc (rand-int (count a))]
     (update a loc flip)))
@@ -51,7 +54,8 @@
 
 (let [a (fn [a _] a)
       b (fn [_ b] b)
-      r #(rand-nth [a b])]
+      r #(rand-nth [a b])
+      flip {1 0, 0 1}]
   ;; aaabbbbbbb
   ;; aaaaaaabbb
   (defn simple-cross []
@@ -77,7 +81,16 @@
   ;; aabbaabbaa
   (def stripe-2 (stripe-cross 2))
   ;; aaabbbaaab
-  (def stripe-3 (stripe-cross 3)))
+  (def stripe-3 (stripe-cross 3))
+
+  ;; aaaaaaaāaaaaa
+  (defn mutate []
+    (->> identity
+         (repeat d/len)
+         vec
+         (#(assoc % (rand-int d/len) {0 1, 1 0}))
+         (map #(fn [a _] (% a)))))
+  (mutate))
 
 (defn cross [f a b]
   (let [ac (:choices a)
@@ -87,8 +100,8 @@
     (create-specimen (mapv #(%1 %2 %3) fc ac bc))))
 
 (defn choose-weighted [xs]
-  ;; takes {weight value} map, values need to be ints, positive
-  (let [choice (->> xs vals (reduce +) (* (rand)))]
+  ;; takes {weight value} map, values should be non-negative
+  (let [choice (->> xs (map second) (reduce +) (* (rand)))]
     (loop [[[k v] & xs] xs
            rem choice]
       (let [rem (- rem v)]
@@ -96,14 +109,24 @@
           (recur xs (- rem v))
           k)))))
 
-(defn roulette [population])
-(defn advance [{:keys [size] :as conf} state]
-  (into #{} (repeatedly size #(apply cross random-cross  (select-n state 2  first)))))
+(defn roulette [population]
+  (choose-weighted (map (juxt identity :score) population)))
+
+(defn advance [{:keys [size cross-fns] :as conf} state]
+  (into #{} (repeatedly size #(apply cross
+                                     (choose-weighted cross-fns)
+                                     (select-n state 2 roulette)))))
 
 (defn simulate [{:keys [size duration] :as conf}]
   (let [initial-state (create-initial-population size)]
     (take  duration (iterate (partial advance conf) initial-state))))
 
-(defn stat [state] [(count state) (/ (reduce + (map :score state)) (count state))])
-
-(map stat (simulate {:size 30 :duration 50}))
+(let [data (map  (partial map :score) (simulate {:size 50
+                                                 :duration 200
+                                                 :cross-fns  {mutate 3
+                                                              simple-cross 3
+                                                              random-cross 3}}))]
+  (view (let [plot (box-plot [])]
+          (doseq [x data]
+            (add-box-plot plot x))
+          plot)))
