820b0471eefdba68eecb4d22fb78ffd00d5fc259
bestestest kitteh <236551@edu.p.lodz.pl>
2022-10-22 20:02:35 +0200

    ogarnięcie konfiguracji

--- src/mh1/alg.clj
+++ src/mh1/alg.clj
@@ -7,18 +7,26 @@
 
 (defrecord specimen [choices weight value valid id])                      ; the thing we are evolving
 
-(defn create-specimen [{:keys [merge-identical]} choices]
+(def ^:dynamic population-size)
+(def ^:dynamic duration)
+(def ^:dynamic replacement-rate)
+(def ^:dynamic merge-identical)
+(def ^:dynamic scoring-fn)
+(def ^:dynamic distribution-fn)
+(def ^:dynamic cross-fns)
+
+(defn create-specimen [choices]
   {:pre (= d/len (count choices))}
   (let [sum-by (fn [sel] (->> choices (map #(* (sel %1) %2) d/items) (reduce +)))
         weight (sum-by :weight)
         value (sum-by :value)]
     (->specimen choices weight value (>= d/max-weight weight) (if merge-identical :merge (rand)))))
 
-(defn spawn-orphan [conf]
-  (create-specimen conf (repeatedly d/len #(rand-int 2))))
+(defn spawn-orphan []
+  (create-specimen (repeatedly d/len #(rand-int 2))))
 
-(defn create-initial-population [{:keys [size] :as conf}]
-  (into #{} (repeatedly size #(spawn-orphan conf))))
+(defn create-initial-population []
+  (into #{} (repeatedly population-size spawn-orphan)))
 
 (let [a (fn [a & _] a)
       b (fn [_ b] b)
@@ -46,7 +54,6 @@
   ;; 011110001100
   (defn entirely-new []
     (repeat d/len sr))
-
   ;; ababababab
   ;; aabbaabbaa
   ;; aaabbbaaab
@@ -57,7 +64,6 @@
            (apply concat)
            (take d/len)
            constantly)))
-
   ;; aaaāaaaaāaa
   (defn mutate [x]
     (fn [] (shuffle (concat (repeat x flip)
@@ -66,12 +72,12 @@
   (defn flip-all []
     (repeat d/len (fn [a _] (flip a)))))
 
-(defn cross [conf f a b]
+(defn cross [f a b]
   (let [ac (:choices a)
         bc (:choices b)
         fc (f)]
     (assert (= (count ac) (count bc) (count fc)))
-    (create-specimen conf (mapv #(%1 %2 %3) fc ac bc))))
+    (create-specimen (mapv #(%1 %2 %3) fc ac bc))))
 
 (defn sum-vals [vs] (->> vs (map second) (reduce +)))
 
@@ -111,17 +117,17 @@
            (if (= to-be-choosen' 0) results'
                (recur (apply dissoc unchoosen loop-results) to-be-choosen' results')))))))
 
-(defn make-specimen [{:keys [cross-fns] :as conf} ranked-pop]
-  (apply cross conf
+(defn make-specimen [ranked-pop]
+  (apply cross
          (choose-weighted cross-fns)
          (choose-weighted 2 ranked-pop)))
 
-(defn roulette [population scoring-fn]
+(defn roulette [population]
   (->> population
        (map (juxt identity scoring-fn))))
 
-(defn ranked [elements scoring-fn]
-  (->> elements
+(defn ranked [population]
+  (->> population
        (sort-by  scoring-fn <)
        (map-indexed (fn [a b] [b a]))))
 
@@ -129,41 +135,43 @@
   (fn  [{:keys [value valid weight]}]
     (if valid value (max 0.001 (* value x (/ d/max-weight weight))))))
 
-(defn advance [{:keys [size cross-fns distribution-fn scoring-fn step] :as conf} state]
-  (let [ranked (distribution-fn state scoring-fn)
-        survivors (choose-weighted  (- size step) ranked)
-        children (repeatedly step #(make-specimen conf ranked))]
+(defn advance [state]
+  (let [roulette-wheel (distribution-fn state)
+        survivors (choose-weighted  (- population-size replacement-rate) roulette-wheel)
+        children (repeatedly replacement-rate #(make-specimen roulette-wheel))]
     (into survivors children)))
 
-(defn simulate [{:keys [size duration] :as conf}]
-  (let [initial-state (create-initial-population conf)]
-    (take  duration (iterate (partial advance conf) initial-state))))
+(defn simulate [conf]
+  (with-bindings conf
+    (let [initial-state (create-initial-population)]
+      (doall (take duration (iterate advance initial-state))))))
 ;; (let [a (create-specimen (repeat 26 0))
 ;;       b (create-specimen (repeat 26 1))]
 ;;   (println "x")
 ;;   (println a)
 ;;   ;; (println b)
 ;;   (println (cross (mutate 3) a a)))
-(let [cfg {:size 70
-           :duration 5000
-           :step 5
-           :merge-identical true
-           :scoring-fn (comp  #(Math/pow % 5) (allowing 0.9))
-           :distribution-fn  ranked
-           :cross-fns  {(mutate 1) 3
-                        (mutate 2) 2
-                        (mutate 3) 1
-                        simple-cross 3
-                        random-cross 3
-                        cut-cross 3
-                        flip-all 0.5
-                        entirely-new 0.5}}
+(let [cfg {#'population-size 70
+           #'duration 500
+           #'replacement-rate 5
+           #'merge-identical true
+           #'scoring-fn (comp  #(Math/pow % 5) (allowing 0.9))
+           #'distribution-fn  ranked
+           #'cross-fns  {(mutate 1) 3
+                         (mutate 2) 2
+                         (mutate 3) 1
+                         simple-cross 3
+                         random-cross 3
+                         cut-cross 3
+                         flip-all 0.5
+                         entirely-new 0.5}}
       data (->> cfg
                 simulate
-                (partition 50)
+                (partition 10)
                 (map first)
-                (map #(->>  % (filter :valid) (map :value))))]
-  (time (last data))
+                (map #(->>  % (filter :valid) (map :value)))
+                doall
+                time)]
   (println "max:" (apply  max (map (partial apply max) data)))
   (time (let [plot (box-plot [])]
           (doseq [x data]
@@ -173,5 +181,3 @@
             view)))
   :ok)
 ;; (view (histogram (map  (fn [{:keys [weight value]}] (/ value weight)) d/items)))
-(defmacro power [count]
-  ~(fn [x] `(concat [*'] (repeat count x))))
