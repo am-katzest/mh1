971b48c533c1291498a8821338ff37d6fc4bba5b
bestestest kitteh <236551@edu.p.lodz.pl>
2022-11-01 16:54:34 +0100

    it was dumb

--- src/mh1/table.clj
+++ src/mh1/table.clj
@@ -6,7 +6,7 @@
 
 (def ^:dynamic interesting-rows [10 20 50 100 150 200 300 400 600 800 1000])
 ;; (def ^:dynamic interesting-rows [10 20 50 100 150 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000])
-(defn to-percentile [x] (- 100.00 (/ x 136928.87)))
+(defn to-percentile [x] (/ x 136928.87))
 (def ^:dynamic format-entry #(format "%.2f\\%%" %))
 
 (defn make-row [xs] (str (reduce #(str %1 " & " %2) xs) " \\\\\n"))
@@ -24,8 +24,8 @@
                                  row% (map to-percentile row)
                                  cnt (count row)
                                  pct-by #(* 100.0 (/  (count (filter % row%)) cnt))
-                                 bests (pct-by zero?)
-                                 top1% (pct-by #(>= 1 %))]
+                                 bests (pct-by #(== 100 %))
+                                 top1% (pct-by #(<= 99 %))]
                              (cons x (map format-entry [min median max bests top1%]))))
         dataa (map format-row
                    (take-while #(<= % duration) interesting-rows))]
